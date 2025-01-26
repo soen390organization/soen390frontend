49243aa14b7145c51f84b778f41296a7c647e139
Franco <franco.dominguez343@gmail.com>
2025-01-25 22:45:45 -0500

    updating workflow with env script file

--- environmentScript.ts
+++ environmentScript.ts
@@ -1,39 +1,28 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+console.log("running environment building script")
+
 const dir = 'src/environments';
-const localEnv = 'environment.ts';
 const prodEnv = 'environment.prod.ts';
 
-const content = `${process.env['ENVIRONMENT_VARS']}`;
+const content = process.env['ENVIRONMENT_VARS'] || 'Default content';
+const testVar = process.env['TEST_VAR'];
+console.log(testVar)
 
-// Check if the directory exists
-fs.access(dir, fs.constants.F_OK, (err) => {
-  if (err) {
-    // Directory doesn't exist
-    console.log("src doesn't exist, creating now", process.cwd());
-    // Create /src
-    try {
-      fs.mkdirSync(dir, { recursive: true });
-    } catch (error) {
-      console.log(`Error while creating ${dir}. Error is ${error}`);
-      process.exit(1);
+function writeToFile(filePath: string, content: string) {
+  fs.writeFile(filePath, content, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('Successfully wrote to file:', filePath);
     }
-  }
+  });
+}
 
-  // Write to files
-  try {
-    fs.writeFileSync(path.join(dir, localEnv), content);
-    fs.writeFileSync(path.join(dir, prodEnv), content);
-    console.log('Created successfully in', process.cwd());
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true });
+}
 
-    if (fs.existsSync(path.join(dir, localEnv))) {
-      console.log('File is created', path.resolve(dir, localEnv));
-      const str = fs.readFileSync(path.join(dir, localEnv)).toString();
-      console.log(str);
-    }
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-});
+const filePath = path.join(dir, prodEnv);
+writeToFile(filePath, content);
