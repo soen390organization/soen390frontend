4979c88df1a12b3c685fed9a16a3f54a8406a603
Mark Ghaby <markghaby@gmail.com>
2025-01-25 22:48:08 -0500

    still trying to fix env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,16 @@
-import { EnvironmentType } from './environment';
-
 export const environment = {
   production: true,
 } as EnvironmentType;
+
+export type EnvironmentType = {
+  production: boolean;
+  firebaseConfig: {
+    apiKey: string;
+    authDomain: string;
+    databaseURL: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+  };
+};
