9bb3f58f9ff4ac6889a57619bc01aa4a87077984
andymiha <andreimihaescu@hotmail.com>
2025-02-07 18:24:27 -0500

    integrated cypress for system/end-to-end testing and wrote basic tests ^

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:8100", // Change to match your Ionic app's local server port
+    setupNodeEvents(on, config) {
+      // Implement node event listeners here if needed
+    },
+    supportFile: "cypress/support/e2e.ts",
+    video: false, // Disable video recording (optional)
+  },
+});
