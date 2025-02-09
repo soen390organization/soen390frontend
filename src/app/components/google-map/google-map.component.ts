7964c6e0913ae9d76ac1afc7c218025273134288
Jonathan <jonathandominicpasquin@gmail.com>
2025-02-09 02:22:50 -0500

    29 - Put proper api key

--- src/app/components/google-map/google-map.component.ts
+++ src/app/components/google-map/google-map.component.ts
@@ -42,9 +42,8 @@ export class GoogleMapComponent implements AfterViewInit {
     return new Promise((resolve) => {
       (window as any).initMap = () => resolve(); // This is the callback from the script
       const script = document.createElement('script');
-      // AIzaSyApYbY5rSV5IyZJ1WMLDMM-I3M5VZQTC9g
-      // AIzaSyDrpAFoOmJewkHwhFjTa8CyR_ZYGuLnEXc
-      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyApYbY5rSV5IyZJ1WMLDMM-I3M5VZQTC9g&callback=initMap&libraries=geometry,places';
+      // Old api key? - AIzaSyApYbY5rSV5IyZJ1WMLDMM-I3M5VZQTC9g
+      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDrpAFoOmJewkHwhFjTa8CyR_ZYGuLnEXc&callback=initMap&libraries=geometry,places';
       script.async = true;
       script.defer = true;
       document.body.appendChild(script);
