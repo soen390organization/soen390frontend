f06d1c4b0bb235d76610ada26a2470e4979e141d
Jonathan <jonathandominicpasquin@gmail.com>
2025-02-15 14:54:49 -0500

    27 - commented store and placesService in interaction bar and added test

--- src/app/components/interaction-bar/interaction-bar.component.spec.ts
+++ src/app/components/interaction-bar/interaction-bar.component.spec.ts
@@ -1,24 +1,35 @@
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-// import { IonicModule } from '@ionic/angular';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { InteractionBarComponent } from './interaction-bar.component';
 
-// import { InteractionBarComponent } from './interaction-bar.component';
+describe('InteractionBarComponent', () => {
+  let component: InteractionBarComponent;
+  let fixture: ComponentFixture<InteractionBarComponent>;
 
-// describe('InteractionBarComponent', () => {
-//   let component: InteractionBarComponent;
-//   let fixture: ComponentFixture<InteractionBarComponent>;
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(), InteractionBarComponent]  // Move InteractionBarComponent to imports
+    }).compileComponents();
 
-//   beforeEach(waitForAsync(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ InteractionBarComponent ],
-//       imports: [IonicModule.forRoot()]
-//     }).compileComponents();
+    fixture = TestBed.createComponent(InteractionBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
-//     fixture = TestBed.createComponent(InteractionBarComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   }));
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should toggle isExpanded when toggleExpand is called', () => {
+    // Initially, isExpanded should be false
+    expect(component.isExpanded).toBeFalse();
+
+    // Call toggleExpand
+    component.toggleExpand();
+    expect(component.isExpanded).toBeTrue();  // After first call, should be true
+
+    // Call toggleExpand again
+    component.toggleExpand();
+    expect(component.isExpanded).toBeFalse();  // After second call, should be false again
+  });
+});
--- src/app/components/interaction-bar/interaction-bar.component.ts
+++ src/app/components/interaction-bar/interaction-bar.component.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { Store } from '@ngrx/store';
-import { PlacesService } from 'src/app/services/places.service';
-import { selectSelectedCampus } from 'src/app/store/app';
+// import { Store } from '@ngrx/store';
+// import { PlacesService } from 'src/app/services/places.service';
+// import { selectSelectedCampus } from 'src/app/store/app';
 
 @Component({
   selector: 'app-interaction-bar',
@@ -17,22 +17,22 @@ export class InteractionBarComponent implements OnInit {
   isExpanded = false;
 
   constructor(
-    private store: Store,
-    private placesService: PlacesService
+    // private store: Store,
+    // private placesService: PlacesService
   ) {
     
   }
 
   ngOnInit() {
-    this.placesService.isInitialized().subscribe((ready) => {
-      if (ready) {
-        this.store.select(selectSelectedCampus).subscribe(async (campus) => {
+    // this.placesService.isInitialized().subscribe((ready) => {
+    //   if (ready) {
+    //     this.store.select(selectSelectedCampus).subscribe(async (campus) => {
           // Getters from PlacesService
           // const buildingResults = await this.placesService.getCampusBuildings();
           // const posResults = await this.placesService.getPointsOfInterest();
-        });
-      }
-    });
+    //     });
+    //   }
+    // });
   }
 
   toggleExpand() {
