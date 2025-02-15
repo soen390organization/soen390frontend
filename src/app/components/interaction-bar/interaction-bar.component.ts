167d716b9075a360aa8bec5896c466ccec90e78f
Jonathan <jonathandominicpasquin@gmail.com>
2025-02-15 14:34:26 -0500

    27 - Removed not working gesture, logs and commmented service use

--- src/app/components/interaction-bar/interaction-bar.component.ts
+++ src/app/components/interaction-bar/interaction-bar.component.ts
@@ -1,5 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
-import { GestureController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -14,13 +13,10 @@ import { selectSelectedCampus } from 'src/app/store/app';
   templateUrl: './interaction-bar.component.html',
   styleUrls: ['./interaction-bar.component.scss']
 })
-export class InteractionBarComponent implements OnInit, AfterViewInit {
+export class InteractionBarComponent implements OnInit {
   isExpanded = false;
 
   constructor(
-    private gestureCtrl: GestureController,
-    private el: ElementRef,
-    private renderer: Renderer2,
     private store: Store,
     private placesService: PlacesService
   ) {
@@ -31,36 +27,12 @@ export class InteractionBarComponent implements OnInit, AfterViewInit {
     this.placesService.isInitialized().subscribe((ready) => {
       if (ready) {
         this.store.select(selectSelectedCampus).subscribe(async (campus) => {
-          console.log('Selected campus changed:', campus);
-          const buildingResults = await this.placesService.getCampusBuildings();
-          const posResults = await this.placesService.getPointsOfInterest();
-          console.log('Campus Buildings:', buildingResults);
-          console.log('Points of Interest:', posResults);
+          // Getters from PlacesService
+          // const buildingResults = await this.placesService.getCampusBuildings();
+          // const posResults = await this.placesService.getPointsOfInterest();
         });
       }
     });
-  }
-
-  ngAfterViewInit() {
-    const gesture = this.gestureCtrl.create({
-      el: this.el.nativeElement.querySelector('.swipe-footer-container'),
-      gestureName: 'swipe-footer',
-      onMove: (detail) => {
-        if (this.isExpanded) return;
-        const offset = Math.max(0, -detail.deltaY);
-        this.renderer.setStyle(
-          this.el.nativeElement.querySelector('.swipe-footer-container'),
-          'height',
-          `${300 + offset}px`
-        );
-      },
-      onEnd: (detail) => {
-        const shouldExpand = detail.deltaY < -50;
-        this.isExpanded = shouldExpand;
-      },
-    });
-
-    gesture.enable(true);
   }
 
   toggleExpand() {
