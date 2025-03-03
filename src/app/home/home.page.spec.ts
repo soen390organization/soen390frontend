83711dc2868cc9a59e17362363161b60887bb8e5
Giancarlo <g_biasiucci@outlook.com>
2025-03-02 23:33:59 -0500

    added tests for home page to hopefully increase code coverage to acceptable level

---
+++ src/app/home/home.page.spec.ts
@@ -0,0 +1,50 @@
+import { HomePage } from 'src/app/home/home.page';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, EventEmitter } from '@angular/core';
+
+
+describe('Home Page', () => {
+    // class MockHomePageFunctionality {
+    //     showSearch = jasmine.createSpy('showSearch');
+    //     hideSearch = jasmine.createSpy('hideSearch');
+    // }
+
+    let homePageSpy: jasmine.SpyObj<HomePage>;
+    let homePage: HomePage;
+    let fixture: ComponentFixture<HomePage>;
+
+    beforeEach(async () => {
+        homePageSpy = jasmine.createSpyObj('HomePage', [
+            'showSearch',
+            'hideSearch'
+        ]);
+
+        await TestBed.configureTestingModule({
+            
+        });
+    });
+    
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HomePage);
+        homePage = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create the home page', () => {
+        expect(homePage).toBeTruthy();
+    });
+
+    describe('State change tests', () => {
+        it('should toggle search visibility', () => {
+            // Initially false
+            expect(homePage.isSearchVisible).toBeFalse();
+      
+            homePage.showSearch();
+            expect(homePage.isSearchVisible).toBeTrue();
+      
+            homePage.hideSearch();
+            expect(homePage.isSearchVisible).toBeFalse();
+        });
+    });
+});
