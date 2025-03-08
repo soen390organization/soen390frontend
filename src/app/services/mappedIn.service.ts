dac7256c8b5511b26be04a60e0a19b121cff27a9
Jonathan <jonathandominicpasquin@gmail.com>
2025-03-07 22:56:46 -0500

    Fixed switch map button, mappedIn service additions

--- src/app/services/mappedIn.service.ts
+++ src/app/services/mappedIn.service.ts
@@ -15,7 +15,40 @@ export class MappedinService {
 
   async initializeMap(container: HTMLElement): Promise<void> {
     const mapData = await this.getMapData();
+    console.log('Map data: ', mapData)
+
     this.mapView = await this.show3dMap(container, mapData);
+    // this.mapView.Labels.all();
+    mapData.getByType('space').forEach((space) => {
+      if (space.name) {
+        this.mapView.Labels.add(space, space.name, { interactive: true });
+      }
+    });
+
+    mapData.getByType('point-of-interest').forEach((space) => {
+      if (space.name) {
+        this.mapView.Labels.add(space, space.name, { interactive: true });
+      }
+    });
+
+    mapData.getByType("connection").forEach((connection) => {
+      console.log(connection)
+      let label = connection.name;
+      // Find the coordinates for the current floor.
+      const coords = connection.coordinates.find(
+        (coord) => coord.floorId === this.mapView.currentFloor.id
+      );
+      // Label the connection.
+      if (connection.type == 'stairs') {
+        label = 'Stairs';
+      } else if (connection.type === 'elevator') {
+        label = 'Elevator';
+      }
+
+      if (coords) {
+        this.mapView.Labels.add(coords, label);
+      }
+    });
   }
 
   /**
