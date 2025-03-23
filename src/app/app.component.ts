989e90c08af19aa6586f77c83d28f3fa772f84ec
andymiha <andreimihaescu@hotmail.com>
2025-03-23 14:44:52 -0400

    polished logrocket implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import LogRocket from 'logrocket';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +6,6 @@ import LogRocket from 'logrocket';
   styleUrls: ['app.component.scss'],
   standalone: false
 })
-export class AppComponent implements OnInit {
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    LogRocket.identify('Randy', {
-      name: 'Andrei Mihaescu',
-      email: 'andreimihaescu@hotmail.com'
-      // Additional custom properties can go here
-    });
-
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        LogRocket.log('Navigated to: ' + event.urlAfterRedirects);
-      }
-    });
-  }
+export class AppComponent {
+  constructor() {}
 }
