0ee9bda2e8d26b20d6b444b3324aa02c6cb6e4a0
Jonathan <jonathandominicpasquin@gmail.com>
2025-04-02 19:03:08 -0400

    Directions Component Test Fix

--- src/app/components/directions/directions.component.ts
+++ src/app/components/directions/directions.component.ts
@@ -1,15 +1,39 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, ViewChild, ElementRef } from '@angular/core';
-import { Step } from 'src/app/interfaces/step.interface';
-import { CurrentLocationService } from 'src/app/services/current-location/current-location.service';
-import { OutdoorDirectionsService } from 'src/app/services/outdoor-directions/outdoor-directions.service';
-import { IconMapping } from 'src/app/interfaces/Icon-mapping';
+import {
+  CommonModule
+} from '@angular/common';
+import {
+  Component,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  AfterViewInit
+} from '@angular/core';
+import {
+  Step
+} from 'src/app/interfaces/step.interface';
+import {
+  CurrentLocationService
+} from 'src/app/services/current-location/current-location.service';
+import {
+  OutdoorDirectionsService
+} from 'src/app/services/outdoor-directions/outdoor-directions.service';
+import {
+  IconMapping
+} from 'src/app/interfaces/Icon-mapping';
 import rawIconMapping from 'src/assets/icon-mapping.json';
-import { NavigationCoordinatorService } from 'src/app/services/navigation-coordinator.service';
-import { CompleteRoute } from 'src/app/interfaces/routing-strategy.interface';
-import { Store } from '@ngrx/store';
-import { setShowRoute } from 'src/app/store/app';
-import { OutdoorDrivingStrategy, OutdoorShuttleStrategy, OutdoorTransitStrategy, OutdoorWalkingStrategy } from 'src/app/strategies/outdoor-directions';
+import {
+  Store
+} from '@ngrx/store';
+import {
+  setShowRoute
+} from 'src/app/store/app';
+import {
+  OutdoorDrivingStrategy,
+  OutdoorShuttleStrategy,
+  OutdoorTransitStrategy,
+  OutdoorWalkingStrategy
+} from 'src/app/strategies/outdoor-directions';
+import { AbstractOutdoorStrategy } from 'src/app/strategies/outdoor-directions/abstract-outdoor.strategy';
 
 const iconMapping = rawIconMapping as IconMapping;
 
@@ -19,9 +43,10 @@ const iconMapping = rawIconMapping as IconMapping;
   selector: 'app-directions',
   templateUrl: './directions.component.html',
   styleUrls: ['./directions.component.scss'],
-  imports: [CommonModule]
+  imports: [CommonModule],
+  standalone: true,
 })
-export class DirectionsComponent implements OnDestroy {
+export class DirectionsComponent implements AfterViewInit, OnDestroy {
   @ViewChild('directionsContainer') directionsContainer!: ElementRef;
 
   steps: Step[] = [];
@@ -29,14 +54,13 @@ export class DirectionsComponent implements OnDestroy {
   selectedMode: string = 'WALKING';
   isLoading: boolean = false;
   currentWatchId: string | null = null;
-  hasArrived: boolean = false; // New boolean to track arrival
+  hasArrived: boolean = false;
   showAllSteps: boolean = true;
   private endNavigationSubscription: any;
   currentRouteData: { eta: string | null; distance: number } | null = null;
 
-  travelModes: any[] = [];
-
   private readonly stepCompletionThreshold = 30;
+  private observer!: IntersectionObserver;
 
   constructor(
     private store: Store,
@@ -45,58 +69,56 @@ export class DirectionsComponent implements OnDestroy {
     public readonly outdoorDrivingStrategy: OutdoorDrivingStrategy,
     public readonly outdoorTransitStrategy: OutdoorTransitStrategy,
     public readonly outdoorShuttleStrategy: OutdoorShuttleStrategy,
-    private currentLocationService: CurrentLocationService,
-    private navigationCoordinator: NavigationCoordinatorService
+    private currentLocationService: CurrentLocationService
   ) {}
-  // ngAfterViewInit(): void {
-    // this.observeComponentPosition();
-  // }
 
-  public getTravelModes() {
-    const travelModes = [
-      { mode: 'WALKING', icon: 'directions_walk', strategy: this.outdoorWalkingStrategy },
-      { mode: 'DRIVING', icon: 'directions_car', strategy: this.outdoorDrivingStrategy },
-      { mode: 'TRANSIT', icon: 'directions_bus', strategy: this.outdoorTransitStrategy },
-      { mode: 'SHUTTLE', icon: 'directions_transit', strategy: this.outdoorShuttleStrategy }
-    ];
-    return travelModes.filter(item => item.strategy?.routes?.length);
+  ngOnInit(): void {
+    this.outdoorDirectionsService.getSelectedStrategy$().subscribe((strategy) => {
+      if (strategy) {
+        this.steps = strategy.getTotalSteps();
+        this.startWatchingLocation();
+      }
+    });
   }
 
-  /**
-   * Continuously checks the component's Y position and updates `showAllSteps`.
-   */
+  ngAfterViewInit(): void {
+    this.observeComponentPosition();
+  }
+
+  public setStrategy(strategy: AbstractOutdoorStrategy) {
+    this.outdoorDirectionsService.clearNavigation();
+    this.outdoorDirectionsService.setSelectedStrategy(strategy);
+    this.outdoorDirectionsService.renderNavigation();
+  }
+
   private observeComponentPosition(): void {
-    const updatePosition = () => {
-      this.updateShowAllSteps();
-      requestAnimationFrame(updatePosition); // Efficient continuous tracking
-    };
+    this.observer = new IntersectionObserver(([entry]) => {
+      const triggerPoint = window.innerHeight / 2;
+      this.showAllSteps = entry.boundingClientRect.top < triggerPoint;
+    }, {
+      threshold: [0, 1.0],
+    });
 
-    requestAnimationFrame(updatePosition);
+    if (this.directionsContainer?.nativeElement) {
+      this.observer.observe(this.directionsContainer.nativeElement);
+    }
   }
 
-  /**
-   * Updates `showAllSteps` based on the component's Y position relative to the viewport.
-   */
-  private updateShowAllSteps(): void {
-    if (!this.directionsContainer) return;
-
-    const componentRect = this.directionsContainer.nativeElement.getBoundingClientRect();
-    const screenHeight = window.innerHeight;
-    const triggerPoint = screenHeight / 2; // Adjust this threshold if needed
-
-    this.showAllSteps = componentRect.top < triggerPoint;
-  }
   ngOnDestroy(): void {
     if (this.currentWatchId) {
       this.currentLocationService.clearWatch(this.currentWatchId);
     }
+
     if (this.endNavigationSubscription) {
       this.endNavigationSubscription.unsubscribe();
+    }
+
+    if (this.observer && this.directionsContainer?.nativeElement) {
+      this.observer.unobserve(this.directionsContainer.nativeElement);
     }
   }
 
   startWatchingLocation(): void {
-    // Use your custom service to watch the user's location.
     this.currentLocationService
       .watchLocation((position: { lat: number; lng: number }) => {
         this.onPositionUpdate(position);
@@ -130,10 +152,9 @@ export class DirectionsComponent implements OnDestroy {
     }
   }
 
-  // Haversine formula to calculate the distance between two points in meters.
   calculateDistance(lat1: number, lng1: number, lat2: number, lng2: number): number {
     const toRad = (x: number) => (x * Math.PI) / 180;
-    const R = 6371e3; // Earth's radius in meters
+    const R = 6371e3;
     const φ1 = toRad(lat1);
     const φ2 = toRad(lat2);
     const Δφ = toRad(lat2 - lat1);
@@ -148,72 +169,37 @@ export class DirectionsComponent implements OnDestroy {
     return R * c;
   }
 
-  async loadDirections(mode: string) {
-    this.isLoading = true;
-    this.hasArrived = false;
-    const start = await this.outdoorDirectionsService.getStartPoint();
-    const destination = await this.outdoorDirectionsService.getDestinationPoint();
-    if (!start || !destination) {
-      console.error('Missing start or destination.');
-      this.isLoading = false;
-      return;
-    }
-
-    try {
-      // Directly use the objects obtained from the getters.
-      const completeRoute: CompleteRoute = await this.navigationCoordinator.getCompleteRoute(
-        start,
-        destination,
-        mode
-      );
-      const { steps, eta } = completeRoute.segments[0].instructions;
-      this.steps = steps;
-      this.eta = eta;
-    } catch (error) {
-      console.error('Failed to fetch directions:', error);
-    } finally {
-      this.isLoading = false;
-    }
-  }
-
-  /**
-   * Updates the travel mode and loads new hardcoded directions.
-   */
-  async setMode(mode: string, event?: Event) {
-    if (event) event.stopPropagation();
-    this.selectedMode = mode;
-    this.loadDirections(mode);
-  }
-
   getDirectionIcon(instructions: string): string {
-    // Remove HTML tags
-    // Create a new DOM parser
     const parser = new DOMParser();
-    // Parse the instructions as HTML
     const doc = parser.parseFromString(instructions, 'text/html');
-    // Extract the text content, which removes all HTML tags
     const plainText = doc.body.textContent || '';
     const lowerText = plainText.toLowerCase();
 
-    // Check directions mapping.
     for (const key in iconMapping.directions_to_symbols) {
       if (lowerText.includes(key)) {
         return iconMapping.directions_to_symbols[key];
       }
     }
 
-    // Check transit mapping.
     for (const key in iconMapping.transit_to_symbols) {
       if (lowerText.includes(key)) {
         return iconMapping.transit_to_symbols[key];
       }
     }
 
-    // Fallback icon.
     return 'help_outline';
   }
 
   onEndClick(): void {
     this.store.dispatch(setShowRoute({ show: false }));
+  }
+
+  getTravelModes() {
+    return [
+      { mode: 'WALKING', icon: 'directions_walk', strategy: this.outdoorWalkingStrategy },
+      { mode: 'DRIVING', icon: 'directions_car', strategy: this.outdoorDrivingStrategy },
+      { mode: 'TRANSIT', icon: 'directions_bus', strategy: this.outdoorTransitStrategy },
+      { mode: 'SHUTTLE', icon: 'directions_transit', strategy: this.outdoorShuttleStrategy }
+    ].filter(item => item.strategy?.routes?.length);
   }
 }
