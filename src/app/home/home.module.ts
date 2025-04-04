20dd453967877b4028a817179995ede56832e198
Sarah <sarahmalik.smm@gmail.com>
2025-04-04 03:29:21 -0400

    small line added to home.ts for compilation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -26,6 +26,7 @@ import { UserProfileComponent } from '../components/user-profile/user-profile.co
     UserProfileComponent
   ],
   declarations: [HomePage],
-  exports: [GoogleMapComponent]
+  exports: [GoogleMapComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule {}
