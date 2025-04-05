9779988a559a7f375fb1e68dad531f65fc58ac5c
nouratabbara <nouratabara@gmail.com>
2025-04-05 16:19:35 -0400

    fixed button functionality to include accessibility

--- src/app/components/accessibility-button/accessibility-button.component.ts
+++ src/app/components/accessibility-button/accessibility-button.component.ts
@@ -1,15 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { IndoorDirectionsService } from 'src/app/services/indoor-directions/indoor-directions.service';
 
 @Component({
   selector: 'app-accessibility-button',
   templateUrl: './accessibility-button.component.html',
-  styleUrls: ['./accessibility-button.component.scss']
+  styleUrls: ['./accessibility-button.component.scss'],
+  imports: [CommonModule]
 })
 export class AccessibilityButtonComponent implements OnInit {
   constructor(public readonly indoorDirectionsService: IndoorDirectionsService) {}
 
   ngOnInit() {}
+
+  async getAccessibility() {
+    const strategy = await this.indoorDirectionsService.getSelectedStrategy();
+    return strategy.accessibility;
+  }
 
   async toggleAccessibility() {
     const strategy = await this.indoorDirectionsService.getSelectedStrategy();
