6a7ff9b75018737814768431f789d8edfa2cd9ef
nouratabbara <nouratabara@gmail.com>
2025-04-05 17:07:52 -0400

    fixed ionic tests

--- src/app/components/accessibility-button/accessibility-button.component.ts
+++ src/app/components/accessibility-button/accessibility-button.component.ts
@@ -8,7 +8,7 @@ import { IndoorDirectionsService } from 'src/app/services/indoor-directions/indo
   styleUrls: ['./accessibility-button.component.scss'],
   imports: [CommonModule]
 })
-export class AccessibilityButtonComponent implements OnInit {
+export class AccessibilityButtonComponent {
   constructor(public readonly indoorDirectionsService: IndoorDirectionsService) {}
 
   async toggleAccessibility() {
